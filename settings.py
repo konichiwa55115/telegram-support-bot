632183314615189afdee0d57c03ecba4e41d2b4c
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-11 06:49:52 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,17 +4,17 @@ from dotenv import load_dotenv, find_dotenv
 # Loading .env variables
 load_dotenv(find_dotenv())
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = "5480351563:AAF4tVGfnAmWR90ttCAfhRLOU_hW8kFCeyc"
 if TELEGRAM_TOKEN is None:
     raise Exception("Please setup the .env variable TELEGRAM_TOKEN.")
 
 PORT = int(os.environ.get('PORT', '8443'))
-HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+HEROKU_APP_NAME = "message62226626264898189"
 
-TELEGRAM_SUPPORT_CHAT_ID = os.getenv("TELEGRAM_SUPPORT_CHAT_ID")
+TELEGRAM_SUPPORT_CHAT_ID = 17983098
 if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip("-").isdigit():
     raise Exception("You need to specify 'TELEGRAM_SUPPORT_CHAT_ID' env variable: The bot will forward all messages to this chat_id. Add this bot https://t.me/ShowJsonBot to your private chat to find its chat_id.")
-TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
+TELEGRAM_SUPPORT_CHAT_ID = 17983098
 
 
 WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "👋")
